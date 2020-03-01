921b6828e96eb9a26023f905bc5715f0d97f953e
Matt Shepardson <lupemls@yahoo.com>
2020-03-01 02:25:29 -0800

    Added code to use the render file to render html and then write it to an html file, checking to see if the output folder it will go into exists, creating it if not

--- app.js
+++ app.js
@@ -28,22 +28,18 @@ const memberPrompt = (position) => {
         switch (response.role){
             case 'Manager':
                 askInfo('Manager');
-                // employee = new Manager(responses.name, responses.id, responses.email, responses.moreInfo);
                 break;
             case 'Engineer':
                 askInfo('Engineer');
-                // employee = new Engineer(responses.name, responses.id, responses.email, responses.moreInfo);
                 break;
             case 'Intern':
                 askInfo('Intern');
-                // employee = new Intern(responses.name, responses.id, responses.email, responses.moreInfo); 
                 break;
             default :
-                console.log(`List of employees: \n\n ${JSON.stringify(employees, null, 2)}`);
-
+                writeRenderedHTML();
+                // console.log(`List of employees: \n\n ${JSON.stringify(employees, null, 2)}`);
                 break;
         };
-        // employees.push(employee);
     });
 };
 memberPrompt();
@@ -77,7 +73,9 @@ const askInfo = (role) => {
             name : 'moreInfo'
         }
     ]).then(function(responses){
-        console.log(responses);
+        // console.log(responses);
+
+        //creates a new employee based on the given role, then adds it to the array of employees
         if(role === 'Manager'){
             employee = new Manager(responses.name, responses.id, responses.email, responses.moreInfo);
         }else if(role === 'Engineer'){
@@ -89,22 +87,21 @@ const askInfo = (role) => {
         memberPrompt();
     })
 };
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+//renders the html from the array of employees, and writes it to a new html file
+const writeRenderedHTML = () => {
+    //renders html using templates of the employees input by the user
+    const employeesHTML = render(employees);
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    //checks to see if the output folder exists, if not, it creates it
+    if(!fs.existsSync('./output')){
+        fs.mkdirSync('./output');
+    }
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+    //writes the html file using the rendered html
+    fs.writeFile(outputPath, employeesHTML, (err) => {
+        if(err){
+            throw err;
+        };
+    });
+};
